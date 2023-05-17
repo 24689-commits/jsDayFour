efc1d6cb624de84d78238a22cf879667c0f777db
Aphelele2 <aphelelejoyi@gmail.com>
2023-05-17 16:20:20 +0200

    Dayfour

---
+++ script/code.js
@@ -0,0 +1,16 @@
+
+
+let person1 = {
+    name: "Aphelele",
+    age: 22 ,
+    favouriteColor: "Blue"
+}
+console.log(person1);
+let person2 = {
+    name: "Que",
+    age: 23,
+    favouriteColor: "black"
+}
+console.log(person2);
+let peopleArray = [person1, person2];
+console.log(peopleArray);
